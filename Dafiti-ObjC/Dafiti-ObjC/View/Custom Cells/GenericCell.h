672e4dfa2ce6643937eab19fd8a3e1d5ba451008
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2015-11-02 20:27:00 -0200

    Created class GenericCell

---
+++ Dafiti-ObjC/Dafiti-ObjC/View/Custom Cells/GenericCell.h
@@ -0,0 +1,13 @@
+//
+//  GenericCell.h
+//  Dafiti-ObjC
+//
+//  Created by Jhonathan Wyterlin on 02/11/15.
+//  Copyright © 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GenericCell : UITableViewCell
+
+@end
---
+++ Dafiti-ObjC/Dafiti-ObjC/View/Custom Cells/GenericCell.m
@@ -0,0 +1,20 @@
+//
+//  GenericCell.m
+//  Dafiti-ObjC
+//
+//  Created by Jhonathan Wyterlin on 02/11/15.
+//  Copyright © 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import "GenericCell.h"
+
+@implementation GenericCell
+
+-(void)awakeFromNib {
+}
+
+-(void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+}
+
+@end
