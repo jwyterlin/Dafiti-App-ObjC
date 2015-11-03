2b8ae2ca7b8958c17da96cdcf037c0ac22e75fd5
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2015-11-03 00:50:25 -0200

    Created class ProductInfoCell

---
+++ Dafiti-ObjC/Dafiti-ObjC/View/Custom Cells/ProductInfoCell.h
@@ -0,0 +1,13 @@
+//
+//  ProductInfoCell.h
+//  Dafiti-ObjC
+//
+//  Created by Jhonathan Wyterlin on 03/11/15.
+//  Copyright © 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProductInfoCell : UITableViewCell
+
+@end
---
+++ Dafiti-ObjC/Dafiti-ObjC/View/Custom Cells/ProductInfoCell.m
@@ -0,0 +1,20 @@
+//
+//  ProductInfoCell.m
+//  Dafiti-ObjC
+//
+//  Created by Jhonathan Wyterlin on 03/11/15.
+//  Copyright © 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import "ProductInfoCell.h"
+
+@implementation ProductInfoCell
+
+-(void)awakeFromNib {
+}
+
+-(void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+}
+
+@end
