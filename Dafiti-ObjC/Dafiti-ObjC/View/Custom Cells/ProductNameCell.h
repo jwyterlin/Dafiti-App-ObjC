97b6ea9286f47c0bee3538b515ff5a49330d89c4
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2015-11-03 00:49:08 -0200

    Created class ProductNameCell.

---
+++ Dafiti-ObjC/Dafiti-ObjC/View/Custom Cells/ProductNameCell.h
@@ -0,0 +1,13 @@
+//
+//  ProductNameCell.h
+//  Dafiti-ObjC
+//
+//  Created by Jhonathan Wyterlin on 03/11/15.
+//  Copyright © 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import "GenericCell.h"
+
+@interface ProductNameCell : GenericCell
+
+@end
---
+++ Dafiti-ObjC/Dafiti-ObjC/View/Custom Cells/ProductNameCell.m
@@ -0,0 +1,20 @@
+//
+//  ProductNameCell.m
+//  Dafiti-ObjC
+//
+//  Created by Jhonathan Wyterlin on 03/11/15.
+//  Copyright © 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import "ProductNameCell.h"
+
+@implementation ProductNameCell
+
+-(void)awakeFromNib {
+}
+
+-(void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+}
+
+@end
